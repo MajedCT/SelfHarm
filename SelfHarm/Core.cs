68e1260c1900e33b971fec0e5aad38b4061693e4
MajedCT <Mjdo0onr@gmail.com>
2025-10-16 10:10:16 +0300

    Add project files.

---
+++ SelfHarm/Core.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using HarmonyLib;
+using Il2CppSLZ.Bonelab;
+using Il2CppSLZ.Marrow;
+using Il2CppSLZ.Marrow.Warehouse;
+using MelonLoader;
+using UnityEngine;
+using BoneLib;
+using Page = BoneLib.BoneMenu.Page;
+
+
+[assembly: MelonInfo(typeof(SelfHarm.Core), "SelfHarm", "1.0.1", "notnotnotswipez", null)]
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
+
+namespace SelfHarm
+{
+
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            base.LoggerInstance.Msg("Initialized.");
+            SetupMelonPrefs();
+            CreateBonemenu();
+        }
+        
+        // full credits to notnotnotswipez for the code, credits to me (MajedCT) for spending an hour adding a toggle button >:D
+        public static void AddImpactProperties(RigManager rigManager)
+        {
+            
+            PhysicsRig physicsRig = rigManager.physicsRig;
+            rigManager.health._testVisualDamage = true;
+            DataCardReference<SurfaceDataCard> surfaceDataCard = new DataCardReference<SurfaceDataCard>("SLZ.Backlot.SurfaceDataCard.Blood");
+            foreach (Rigidbody rigidbody in physicsRig.GetComponentsInChildren<Rigidbody>())
+            {
+                bool flag = rigidbody.GetComponent<ImpactProperties>();
+                if (!flag)
+                {
+                    GameObject gameObject = rigidbody.gameObject;
+                    bool flag2 = gameObject.GetInstanceID() == physicsRig.feet.GetInstanceID();
+                    if (!flag2)
+                    {
+                        bool flag3 = gameObject.GetInstanceID() == physicsRig.knee.GetInstanceID();
+                        if (!flag3)
+                        {
+                            ImpactProperties impactProperties = rigidbody.gameObject.AddComponent<ImpactProperties>();
+                            impactProperties.SurfaceDataCard = surfaceDataCard;
+                            impactProperties.decalType = (ImpactProperties.DecalType)(-1);
+                        }
+                    }
+                }
+            }
+        }
+        public static void DestroyImpactProperties(RigManager rigManager)
+        {
+            PhysicsRig physicsRig = rigManager.physicsRig;
+            rigManager.health._testVisualDamage = true;
+            DataCardReference<SurfaceDataCard> surfaceDataCard = new DataCardReference<SurfaceDataCard>("SLZ.Backlot.SurfaceDataCard.Blood");
+            foreach (Rigidbody rigidbody in physicsRig.GetComponentsInChildren<Rigidbody>())
+            {
+                bool flag = rigidbody.GetComponent<ImpactProperties>();
+                if (flag)
+                {
+                    // DESTROY THEM DESTROY THE IMPACT PROPERTIES DIE
+                    ImpactProperties.Destroy(rigidbody.GetComponent<ImpactProperties>());
+
+                }
+            }
+        }
+
+
+        [HarmonyPatch(typeof(UIRig), "Awake")]
+        public class UIRigAwakePatch
+        {
+            
+            public static void Postfix()
+            {
+                if (isEnabled)
+                {
+                    MelonLogger.Msg("[Self Harm] Mod is already eanbled, Adding impact properties");
+                    AddImpactProperties(PlayerRefs.Instance.PlayerRigManager);
+                }
+            }
+        }
+
+        
+        
+        public static MelonPreferences_Category MelonPrefCategory
+        {
+            get; set;
+        }
+        public static MelonPreferences_Entry<bool> ModEnabledPref
+        {
+            get; set;
+        }
+        public static bool isEnabled
+        {
+            get; set;
+        }
+
+
+        public static void SetupMelonPrefs()
+        {
+            MelonPrefCategory = MelonPreferences.CreateCategory("Self Harm");
+            ModEnabledPref = MelonPrefCategory.CreateEntry("isEnabled", true);
+            isEnabled = ModEnabledPref.Value;
+
+
+        }
+        public static void CreateBonemenu()
+        {
+            Page mainPage = Page.Root.CreatePage("Self Harm", Color.red);
+            mainPage.CreateBool("Mod Toggle", Color.yellow, ModEnabledPref.Value, OnToggle);
+        }
+
+        public static void OnToggle(bool value)
+        {
+            isEnabled = value;
+
+            if (isEnabled)
+            {
+                MelonLogger.Msg("[Self Harm] Mod Enabled, Adding Impact Properties.");
+                AddImpactProperties(PlayerRefs.Instance.PlayerRigManager);
+            }
+            else if(!isEnabled)
+            {
+                MelonLogger.Msg("[Self Harm] Mod Disabled, Removing Impact Properties.");
+                DestroyImpactProperties(PlayerRefs.Instance.PlayerRigManager);
+            }
+
+            
+            ModEnabledPref.Value = value;
+            MelonPrefCategory.SaveToFile(true);
+        }
+    }
+}
